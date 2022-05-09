85064f3180a179289288f2b6972f9b6d7eec15d8
Aloha <jun.huang@shanbay.com>
2022-05-10 00:45:56 +0800

    feat: config giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -44,10 +44,12 @@ const siteMetadata = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
+      mapping: 'title', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
+      lang: 'zh-CN',
+      inputPosition: 'top',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
